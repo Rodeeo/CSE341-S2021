c941d80829e6059abea69494370069f4938d2181
Rodeeo <therodee@gmail.com>
2021-04-24 22:46:05 -0600

    Working Users

--- cse341-project-master/prove01-routes.js
+++ cse341-project-master/prove01-routes.js
@@ -1,53 +1,59 @@
-const requestHandler = (req, res) => {
-    const url = req.url;
-    
-    if (url === '/') {
+const fs = require('fs');
+const users = ["User 1","User 2"];
+
+const requestHandler = (req, res)=> {
+  const url = req.url;
+  
+  if (url === '/' ) {
+    res.write('<html>');
+    res.write('<head><title>Prove 01</title></head>');
+    res.write('<body><h3><a href="users">Users</a></h3>');
+    res.write('<h1>Enter a Username</h1>');
+    res.write('<main><form action="/create-user" method="POST">');
+    res.write('<label for="userName">Username: </label><br><input type="text" name="userName" id="userName">');
+    res.write('<button type="submit">Add User</button></form></main>');   
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/users' ) {
+    res.setHeader('Location', '/users');
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><head><title>Users</title></head>');
+    res.write('<body><h3><a href="/">Back</a></h3>');
+    res.write('<h1>List of Users:</h1>');
+    res.write('<main><ul>');
+    console.log(users);
+    for(let i=0;i<users.length; i++){
+    res.write('<li>'+users[i]+'</li>'); }
+    res.write('</ul></main></body></html>');
+    return res.end();
+  }
+
+  if(url === '/create-user'){
+    const body = [];
+    req.on('data', chunk => {
+        body.push(chunk);
+    });
+
+    return req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        users.push(parsedBody.split('=')[1]);
+        res.setHeader('Location', '/users');
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
-        res.write('<head><title>Prove 1</title></head>');
-        res.write('<body><h1>Enter a Username</h1>');
-        res.write('<main><form action="/create-user" method="POST">');
-        res.write('<label for="username">Username: </label><input type="text" name="username" id="username">');
-        res.write('<button type="submit">Send</button></form></main></body>');
-        res.write('</html>');
+        res.write('<head><title>Users</title></head>');
+        res.write('<body><h3><a href="/">Back</a></h3>');
+        res.write('<h1>List of Users:</h1>');
+        res.write('<main><ul>');
+        console.log(users);
+        for(let i=0;i<users.length; i++){
+        res.write('<li>'+users[i]+'</li>');  }
+        res.write('</ul></main></body></html>');
         return res.end();
-    }
+    });
+  }
+}
 
-    if (url === '/create-user') { 
-        const body = [];
-        req.on('data', chunk => {
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody.split('=')[1]);
-            res.setHeader('Location', '/users');
-            res.setHeader('Content-Type', 'text/html');
-            res.write('<html>');
-            res.write('<head><title>Users</title></head>');
-            res.write('<h1>List of Users:</h1>');
-            res.write('<body><ul><li>User 1</li><li>User 2</li>');
-            res.write('<li>');
-            res.write(parsedBody.split('=')[1]);
-            res.write('</li></ul>');
-            res.write('</body>');
-            res.write('</html>');
-            return res.end();
-        }); 
-    }
-        
-    if (url === '/users' ) {
-        res.setHeader('Location', '/users');
-            res.setHeader('Content-Type', 'text/html');
-            res.write('<html>');
-            res.write('<head><title>Users</title></head>');
-            res.write('<h1>List of Users:</h1>');
-            res.write('<body><ul>');
-            res.write('<li>User 1</li><li>User 2</li>');
-            res.write('</ul></body>');
-            res.write('</html>');
-            return res.end();
-     }
-    };
-
-exports.handler = requestHandler;
+module.exports = requestHandler;
