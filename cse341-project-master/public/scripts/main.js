2be4d51ab76dda5a7630acdb64a8199291d1daf5
Rodeeo <therodee@gmail.com>
2021-05-27 00:10:44 -0600

    week 6 finshed CSS changes

--- cse341-project-master/public/scripts/main.js
+++ cse341-project-master/public/scripts/main.js
@@ -1,30 +1,9 @@
-const backdrop = document.querySelector('.backdrop');
-const sideDrawer = document.querySelector('.mobile-nav');
-const menuToggle = document.querySelector('#toggleMenu');
-
-function backdropClickHandler() {
-  backdrop.style.display = 'none';
-  sideDrawer.classList.remove('open');
-}
-
-function menuToggleClickHandler() {
-  backdrop.style.display = 'block';
-  sideDrawer.classList.add('open');
-}
-
-backdrop.addEventListener('click', backdropClickHandler);
-menuToggle.addEventListener('click', menuToggleClickHandler);
-
-
-function add2(op1,op2) {
-let add2;
-// Write your code here
-area = op1 + op2;    
-return add2;
-
-}
-
-
-
- 
+(function() {
+  var burger = document.querySelector('.burger');
+  var menu = document.querySelector('#'+burger.dataset.target);
+  burger.addEventListener('click', function() {
+      burger.classList.toggle('is-active');
+      menu.classList.toggle('is-active');
+  });
+})();
 
