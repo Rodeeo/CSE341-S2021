ccea4dbf80f972b1f8c46df0bc15ae7da2bb768c
Rodeeo <therodee@gmail.com>
2021-06-12 02:58:42 -0600

    working

---
+++ proves/app.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+const app = express();
+
+const corsOptions = {
+    origin: "https://prove-rodee-cse341.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(shopRoutes);
+
+app.listen(PORT);
